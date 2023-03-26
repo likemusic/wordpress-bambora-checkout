cd1adf231bfa3e9a8398bc803b7a92449101ac5a
Valery Ivashchanka <likemusicdev@gmail.com>
2023-03-26 18:45:53 +0300

    Initial commit

---
+++ src/BamboraPlugin.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Affairsoft\Wordpress\Bambora;
+
+use JetBrains\PhpStorm\NoReturn;
+use Likemusic\BamboraCheckout\Parameters\LinkParameters;
+
+class BamboraPlugin
+{
+    public function __construct()
+    {
+        add_action('admin_menu', [$this, 'addAdminMenu']);
+        add_action('parse_request', [$this, 'parseRequestHandler']);
+    }
+
+    public function parseRequestHandler(): void
+    {
+        $requestedPath = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+        $config = get_option('bambora');
+
+        $paymentUrl = $config['payment_url'];
+
+        if ($requestedPath !== $paymentUrl) {
+            return;
+        }
+
+        $knownParams = $this->getKnownParams($_REQUEST);
+
+        $paymentUrlGenerator = new ConfiguredPaymentUrlGenerator();
+        $paymentUrl = $paymentUrlGenerator->makeByArray($knownParams);
+
+        $this->redirect($paymentUrl);
+
+        exit();
+    }
+
+    private function getKnownParams(array $arr): array
+    {
+        $knownParametersNames = LinkParameters::getNames();
+
+        return array_intersect_key($arr, array_fill_keys($knownParametersNames, null));
+    }
+
+
+    #[NoReturn]
+    private function redirect($url): void
+    {
+        $this->redirectByLocation($url);
+        $this->renderRedirectPage($url);
+    }
+
+    #[NoReturn]
+    private function redirectByLocation($url, $permanent = false): void
+    {
+        header('Location: ' . $url, true, $permanent ? 301 : 302);
+    }
+
+    private function renderRedirectPage($url): void
+    {
+        $escapesUrl = htmlspecialchars($url);
+
+        $jsonUrl = json_encode($url);
+
+        echo "<meta http-equiv=\"refresh\" content=\"0;url={$escapesUrl}\">";
+
+        echo "<script>window.location.href={$jsonUrl};</script>";
+        echo "<noscript>If you will not redirect to payment page please go to link: <a href=\"{$escapesUrl}\">{$url}</a></noscript>";
+    }
+
+    public function addAdminMenu(): void
+    {
+        add_menu_page('Bambora.com Payment', 'Bambora.com Payment', 'manage_options',
+            'bambora', [$this, 'adminPageProcessor'], 'dashicons-money-alt'
+        );
+    }
+
+    public function adminPageProcessor(): void
+    {
+        $this->processPost();
+        $this->renderAdminPage();
+    }
+
+    private function processPost(): void
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            return;
+        }
+
+        $newConfig = [
+            'merchant_id' => $_POST['merchant_id'],
+            'hash_key' => $_POST['hash_key'],
+            'payment_url' => $_POST['payment_url']
+        ];
+
+        update_option('bambora', $newConfig);
+    }
+
+    private function renderAdminPage(): void
+    {
+        $config = get_option('bambora');
+
+        echo <<<CONTENT
+<div class="wrap">
+    <h1>Bambora.com Payment Settings</h1>
+    <form method="post">
+        <table class="form-table" role="presentation">
+            <tbody>
+                <tr>
+                    <th scope="row"><label for="merchant_id">Merchant ID</label></th>
+                    <td>
+                        <input name="merchant_id" type="text" id="merchant_id" value="{$config['merchant_id']}" class="regular-text">
+                        <p class="description" id="merchant_id-description">Go to <a href="https://web.na.bambora.com/">Bambora's member area</a> to get it.</p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th scope="row"><label for="hash_key">Hash Key</label></th>
+                    <td>
+                        <input name="hash_key" type="text" id="hash_key" value="{$config['hash_key']}" class="regular-text">
+                        <p class="description" id="hash_key-description">Go to <a href="https://web.na.bambora.com/">Bambora's member area</a> to get it.</p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th scope="row"><label for="payment_url">Payment URL</label></th>
+                    <td>
+                        <input name="payment_url" type="text" id="payment_url" value="{$config['payment_url']}" class="regular-text">
+                        <p class="description" id="payment_url-description">Url where given request parameters would be used to generate and redirect to payment form. Go to <a href="https://dev.na.bambora.com/docs/references/checkout/">Link parameters docs</a> to see all available parameters.</p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+CONTENT;
+        submit_button(__('Update'), 'primary large', 'save', false);
+
+        echo <<<CONTENT
+</form>
+</div>
+CONTENT;
+    }
+}
---
+++ src/ConfiguredPaymentUrlGenerator.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Affairsoft\Wordpress\Bambora;
+
+use Likemusic\BamboraCheckout\PaymentUrlGenerator;
+
+class ConfiguredPaymentUrlGenerator extends PaymentUrlGenerator
+{
+    public function __construct(
+        // Order info
+               $trnAmount = null, $trnOrderNumber = null, $trnType = null, $trnCardOwner = null, $trnLanguage = null,
+
+        // Billing address
+               $ordName = null, $ordEmailAddress = null, $ordAddress1 = null, $ordAddress2 = null, $ordCity = null,
+               $ordProvince = null, $ordPostalCode = null, $ordCountry = null,
+
+        // Shipping address
+               $shipName = null, $shipEmailAddress = null, $shipAddress1 = null, $shipAddress2 = null, $shipCity = null,
+               $shipProvince = null, $shipPostalCode = null, $shipCountry = null, $shipPhoneAddress = null,
+
+        // Redirects
+               $approvedPage = null, $declinedPage = null,
+
+        // Hash expiry
+               $hashExpiry = null,
+
+        // References info
+               $ref1 = null, $ref2 = null, $ref3 = null, $ref4 = null, $ref5 = null,
+
+               $baseUrl = 'https://web.na.bambora.com/scripts/payment/payment.asp'
+    )
+    {
+        $config = get_option('bambora');
+
+        $hashKey = $config['hash_key']; //INSERT API ACCESS PASSCODE
+        $merchantId = $config['merchant_id']; //INSERT MERCHANT ID (must be a 9 digit string)
+
+        parent::__construct(
+            $hashKey,
+
+            // Authorization
+            $merchantId,
+
+            // Order info
+            $trnAmount, $trnOrderNumber, $trnType, $trnCardOwner, $trnLanguage,
+
+            // Billing address
+            $ordName, $ordEmailAddress, $ordAddress1, $ordAddress2, $ordCity, $ordProvince, $ordPostalCode, $ordCountry,
+
+            // Shipping address
+            $shipName, $shipEmailAddress, $shipAddress1, $shipAddress2, $shipCity, $shipProvince, $shipPostalCode, $shipCountry, $shipPhoneAddress,
+
+            // Redirects
+            $approvedPage, $declinedPage,
+
+            // Hash expiry
+            $hashExpiry,
+
+            // References info
+            $ref1, $ref2, $ref3, $ref4, $ref5,
+
+            $baseUrl
+        );
+    }
+
+}
