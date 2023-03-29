bc1bc1f0789a465af20be99ad0c60afb474a8ac2
Valery Ivashchanka <likemusicdev@gmail.com>
2023-03-30 00:48:56 +0300

    Remove custom redirect page handler.

--- src/BamboraCheckoutPlugin.php
+++ src/BamboraCheckoutPlugin.php
@@ -2,70 +2,11 @@
 
 namespace Likemusic\Wordpress\Bambora\Checkout;
 
-use JetBrains\PhpStorm\NoReturn;
-use Likemusic\BamboraCheckout\Parameters\LinkParameters;
-
 class BamboraCheckoutPlugin
 {
     public function __construct()
     {
         add_action('admin_menu', [$this, 'addAdminMenu']);
-        add_action('parse_request', [$this, 'parseRequestHandler']);
-    }
-
-    public function parseRequestHandler(): void
-    {
-        $requestedPath = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-
-        $config = get_option('bambora');
-
-        $paymentUrl = $config['payment_url'];
-
-        if ($requestedPath !== $paymentUrl) {
-            return;
-        }
-
-        $knownParams = $this->getKnownParams($_REQUEST);
-
-        $paymentUrlGenerator = new ConfiguredPaymentUrlGenerator();
-        $paymentUrl = $paymentUrlGenerator->makeByArray($knownParams);
-
-        $this->redirect($paymentUrl);
-
-        exit();
-    }
-
-    private function getKnownParams(array $arr): array
-    {
-        $knownParametersNames = LinkParameters::getNames();
-
-        return array_intersect_key($arr, array_fill_keys($knownParametersNames, null));
-    }
-
-
-    #[NoReturn]
-    private function redirect($url): void
-    {
-        $this->redirectByLocation($url);
-        $this->renderRedirectPage($url);
-    }
-
-    #[NoReturn]
-    private function redirectByLocation($url, $permanent = false): void
-    {
-        header('Location: ' . $url, true, $permanent ? 301 : 302);
-    }
-
-    private function renderRedirectPage($url): void
-    {
-        $escapesUrl = htmlspecialchars($url);
-
-        $jsonUrl = json_encode($url);
-
-        echo "<meta http-equiv=\"refresh\" content=\"0;url={$escapesUrl}\">";
-
-        echo "<script>window.location.href={$jsonUrl};</script>";
-        echo "<noscript>If you will not redirect to payment page please go to link: <a href=\"{$escapesUrl}\">{$url}</a></noscript>";
     }
 
     public function addAdminMenu(): void
@@ -90,7 +31,6 @@ class BamboraCheckoutPlugin
         $newConfig = [
             'merchant_id' => $_POST['merchant_id'],
             'hash_key' => $_POST['hash_key'],
-            'payment_url' => $_POST['payment_url']
         ];
 
         update_option('bambora', $newConfig);
@@ -119,14 +59,6 @@ class BamboraCheckoutPlugin
                     <td>
                         <input name="hash_key" type="text" id="hash_key" value="{$config['hash_key']}" class="regular-text">
                         <p class="description" id="hash_key-description">Go to <a href="https://web.na.bambora.com/">Bambora's member area</a> to get it.</p>
-                    </td>
-                </tr>
-
-                <tr>
-                    <th scope="row"><label for="payment_url">Payment URL</label></th>
-                    <td>
-                        <input name="payment_url" type="text" id="payment_url" value="{$config['payment_url']}" class="regular-text">
-                        <p class="description" id="payment_url-description">Url where given request parameters would be used to generate and redirect to payment form. Go to <a href="https://dev.na.bambora.com/docs/references/checkout/">Link parameters docs</a> to see all available parameters.</p>
                     </td>
                 </tr>
             </tbody>
